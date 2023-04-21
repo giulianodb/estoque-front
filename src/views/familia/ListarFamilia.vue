195ccdd8f53a95c32f4552c4aa738c7f4f798d9f
Giuliano <giulianodb@gmail.com>
2023-04-21 06:09:31 -0300

    Ao pesquisar voltar para página inicial da paginação

--- src/views/familia/ListarFamilia.vue
+++ src/views/familia/ListarFamilia.vue
@@ -238,6 +238,7 @@ export default {
     
     listarFamilias () {
       this.pesquisando = true
+      this.currentPage = 0
       Api.getFamilias(
         this.currentPage,
         this.perPage,
